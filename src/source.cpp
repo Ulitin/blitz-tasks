dd913f32d738448e12aadc7cffc3ad21f6adc894
Ulitin <sashavladimirovichulitin@gmail.com>
2016-12-02 02:01:44 +0300

    task 2

--- src/source.cpp
+++ src/source.cpp
@@ -27,54 +27,35 @@ int convert(int x) {
 
 //задача 2
 void max_in_first_el(int **mas, int i) {
-	int *buf = mas[i];
-	if (mas[0][2] < buf[2]) {
-		int *buf_2 = mas[i];
-		buf = mas[0];
-		mas[0] = buf_2;
+	if (mas[0][2] < mas[i][2]) {
+		int buf;
+		for (int j = 0; j < 3; j++) {
+			buf = mas[i][j];
+			mas[i][j] = mas[0][j];
+			mas[0][j] = buf;
+		}
 	}
 }
 
 int Lenght_input(char *a, char *b) {
-	int i = 0;
-	for (; (a[i] != 0) && (b[i] != 0) && (b[i] = a[i]); i++) { }
+	int i = 1;
+	for (;(b[i] != 0) && (b[i] == a[i]); i++) {}
 	return i;
 }
 int Lenght(char* a) {
 	int i = 0;
 	for (; a[i] != 0; i++) {}
-	return i;
+	return i + 1;
 }
 
 char* find_pref_two_el(char*a, char* b) {
 	int m;
 	int l;
-	if (Lenght(a) < Lenght(b)) {
-		m = Lenght(b) - Lenght(a) + 1;
-	} 
-	else {
-		m = Lenght(a) - Lenght(b) + 1;
-	}
+	m = Lenght(a)*Lenght(b) + 1;
 	int **mas = new int*[m];
-	for (int i = 0; i < m; i++) {
+	for (int i = 0; i < (m); i++) {
 		mas[i] = new int[3];
 	}
-
-	/*
-	int lenght_b, lenght_m;
-	for (int lenght_m = 0; (a[lenght_m] != 0) && (b[lenght_m] != 0); lenght_m++) {
-		if ((a[lenght_m + 1] == 0) && (b[lenght_m + 1] != 0)) {
-			char *buf = a;
-			a = b;
-			b = buf;
-		}
-	}
-	for (int lenght_b = lenght_m; a[lenght_b] != 0; lenght_b++) { }
-	char **pref = new char*[lenght_b - lenght_m + 1];
-	for (int i = 0; i < (lenght_b - lenght_m + 1); i++) {
-		pref[i] = new char [lenght_m];
-	}
-	*/
 
 	int q = 0;
 	for (int i = 0; a[i] != 0; i++) {
@@ -85,23 +66,32 @@ char* find_pref_two_el(char*a, char* b) {
 				mas[q][2] = Lenght_input(a+i, b+j);
 				mas[q + 1][0] = -1;
 				max_in_first_el(mas, q);
+				q++;
 			}
 		}
 	}
 	char *pref = new char[mas[0][2]];
 	for (int j = 0; j < mas[0][2]; j++) {
-		pref[j] = a[mas[0][1] + j];
+		pref[j] = a[mas[0][0] + j];
 	}
+	pref[mas[0][2]] = 0;
 	return pref;
 }
 
-char* find_pref(char**a, int n) {
-	char*pref;
+char* find_pref(char**a, int n) { // a - массив строк, n - b - кол-во строк
+	if ((a[0] == 0) && (a[1] == 0)) {
+		throw 1;
+	}
+	if(n == 1) {
+		return a[0];
+	}
+	char *pref = a[0];
 	int i = 1;
-	if(a[i]==0)
-		return 0;
-	while((a[i] != 0) && (i < n)) {
-		pref = find_pref_two_el(a[i-1], a[i]);
+	while(i < n) {
+		if (a[i] == 0) {
+			throw 1;
+		}
+		pref = find_pref_two_el(pref, a[i]);
 		i++;
 	}
 	return pref;
--- test/test.cpp
+++ test/test.cpp
@@ -17,6 +17,17 @@ TEST(Convert, correct_namber_with_minus)
 
 }
 
+TEST(find_pref_two_el, correct_on_zero)
+{
+	char* a = "aas";
+	char **x;
+	x = new char*[2];
+	*x = a;
+	*(x + 1) = 0;
+	ASSERT_ANY_THROW(find_pref(x, 2));
+}
+
+
 TEST(find_pref_two_el, correct_1)
 {
 	char* a = "aas";
@@ -47,18 +58,18 @@ TEST(find_pref_two_el, correct_2)
 	*x = a;
 	*(x + 1) = b;
 	c = find_pref(x, 2);
-	for (int i = 0; i < 3; i++) {
-		if (a[i] == c[i]) {
+	for (int i = 0; i < 2; i++) {
+		if (b[i] == c[i]) {
 			j++;
 		}
 	}
-	ASSERT_EQ(3, j);
+	ASSERT_EQ(2, j);
 }
 
 TEST(find_pref_two_el, correct_3)
 {
 	char* a = "aas";
-	char* b = "aas";
+	char* b = "las";
 	char* q = "as";
 	char **x;
 	char *c;
@@ -68,12 +79,29 @@ TEST(find_pref_two_el, correct_3)
 	*(x + 1) = b;
 	*(x + 2) = q;
 	c = find_pref(x, 3);
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < 2; i++) {
 		if (q[i] == c[i]) {
 			j++;
 		}
 	}
 	ASSERT_EQ(2, j);
+}
+
+TEST(find_pref_two_el, correct_4)
+{
+	char* a = "aas";
+	char* b = "azs";
+	char* q = "zas";
+	char **x;
+	char *c;
+	int j = 0;
+	x = new char*[3];
+	*x = a;
+	*(x + 1) = b;
+	*(x + 2) = q;
+	c = find_pref(x, 3);
+	for (j; c[j] != 0; j++) {}
+	ASSERT_EQ(1, j);
 }
 
 int main(int ac, char* av[])
